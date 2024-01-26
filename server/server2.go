75198bdf096755f6e54be51bc92f54cb534ef77a
its-me-yps <yps8181@gmail.com>
2024-01-27 00:41:31 +0530

    File re-structure

--- server/server2.go
+++ server/server2.go
@@ -11,31 +11,28 @@ import (
 func Server2(groupedData map[string]model.Hall, rawData model.Students, port string) {
 	r := gin.Default()
 
-	r.GET("/:hall/:wing/:room", func(c *gin.Context) {
-		h := c.Param("hall")
-		w := c.Param("wing")
-		r := c.Param("room")
-
-		if h == "" {
-			c.JSON(200, groupedData)
-		}
-		if hall, ok := groupedData[h]; ok {
-			if w == "" {
-				c.JSON(200, hall)
-			}
-			if wing, ok := hall[w]; ok {
-				if r == "" {
-					c.JSON(200, wing)
-				}
-				if room, ok := wing[r]; ok {
-					c.JSON(200, room)
-					return
-				}
-			}
-		}
-
-		c.JSON(404, gin.H{"error": "Not Found"})
+	r.POST("/wingiesOrNot", func(c *gin.Context) {
+		postReq2(c, rawData)
 	})
-	
-	r.Run(":8080")
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, groupedData)
+	})
+
+	r.GET("/:hall", func(c *gin.Context) {
+		c.Set("wing", "")
+		c.Set("room", "")
+		getReq2(c, groupedData)
+	})
+
+	r.GET("/:hall/:wing", func(c *gin.Context) {
+		c.Set("room", "")
+		getReq2(c, groupedData)
+	})
+
+	r.GET("/:hall/:wing/:room", func(c *gin.Context) {
+		getReq2(c, groupedData)
+	})
+
+	r.Run(":" + port)
 }
